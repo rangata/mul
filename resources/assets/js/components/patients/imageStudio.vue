9c04f3ccdfdbfcffb546646487ffe559eeacba77
rangata <urandom@abv.bg>
2018-07-26 12:10:48 +0300

    Dumb updates

---
+++ resources/assets/js/components/patients/imageStudio.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "imageStudio"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/patients/studio.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: urandom
+ * Date: 26.07.18
+ * Time: 11:21
+ */
